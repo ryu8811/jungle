bb2c1ac5bf97da98d254ef99b1e1eda457e41a21
ryu8811 <ryuu8811@gmail.com>
2021-01-07 21:54:59 +0900

    commit

--- class_6/js/script_ui.js
+++ class_6/js/script_ui.js
@@ -1,4 +1,27 @@
 $(function(){
+    // 자리셋팅 버튼
+    $(".btn_setting").click(function(){
+        $(".section.box_intro").removeClass("on"); //버튼 제거
+        loadDataFn();
+    });
     
-    
+
+    var loadData; // ajax로 불러온 json 데이터를 담는 변수
+
+    function loadDataFn(){
+        $.ajax({
+            url:"js/data.json", // 데이터 경로
+            dataType:"json",
+            success:function(result){ // 성공했을때의 값을 받을 result라는 임의의 변수
+                loadData = result.seatInfo;
+                settingSeatFn();
+                console.load(loadData);
+            }
+        });
+    };
+
+    // 자리 배치
+    function settingSeatFn(){
+
+    }
 })
